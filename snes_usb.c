707f6c59e02aa2ef58115cd83a02d36ccfba7c49
lispandtrees <unixandtrees@gmail.com>
2013-02-13 13:38:40 -0600

    filled skeleton functions and the driver is now able to be loaded

--- snes_usb.c
+++ snes_usb.c
@@ -17,6 +17,13 @@
 #define SNES_USB_BUF_SIZE (1 << 15)
 #define SNES_USB_IFQ_MAX_LEN 2
 
+#define UREQ_GET_PORT_STATUS 0x01
+#define UREQ_SOFT_RESET      0x02
+
+#define SNES_USB_X 0x30
+#define SNES_USB_Y 0x31
+
+
 enum{
 	SNES_USB_INTR_DT_RD,
 	SNES_USB_STATUS_DT_RD,
@@ -85,82 +92,308 @@ static const struct usb_config snes_usb_config[SNES_USB_N_TRANSFER] =
 static int
 snes_usb_open(struct usb_fifo *fifo, int fflags)
 {
-	//todo
-	return 0;
+	struct snes_usb_softc *sc = usb_fifo_softc(fifo);
+	int error;
+	
+	if(sc->sc_fflags == 0)
+		snes_usb_reset(sc);
+	
+	if(fflags & FREAD){
+		mtx_lock(&sc->sc_mutex);
+		usbd_xfer_set_stall(sc->sc_transfer[SNES_USB_INTR_DT_RD]);
+		mtx_unlock(&sc->sc_mutex);
+
+		error = usb_fifo_alloc_buffer(fifo,
+			usbd_xfer_max_len(sc->sc_transfer[SNES_USB_INTR_DT_RD]),
+			SNES_USB_IFQ_MAX_LEN);
+		if(error)
+			return (ENOMEM);
+
+		sc->sc_fifo_open[USB_FIFO_RX] = fifo;
+	}
+	
+	sc->sc_fflags |= fflags & FREAD;
+	return (0);
 }	 
+
 
 static void
 snes_usb_reset(struct snes_usb_softc *sc)
 {
-	//todo
+	struct usb_device_request req;
+	int error;
+		
+	req.bRequest = UREQ_SOFT_RESET;
+	USETW(req.wValue, 0);
+	USETW(req.wIndex, sc->sc_iface_num);
+	USETW(req.wLength, 0);
+
+	mtx_lock(&sc->sc_mutex);
+
+	error = usbd_do_request_flags(sc->sc_usb_device, &sc->sc_mutex,
+			&req, NULL, 0, NULL, 2 * USB_MS_HZ);
+
+	if(error){
+		usbd_do_request_flags(sc->sc_usb_device, &sc->sc_mutex,
+			&req, NULL, 0, NULL, 2 * USB_MS_HZ);
+	}
+
+	mtx_unlock(&sc->sc_mutex);
 }
 
 static void
 snes_usb_close(struct usb_fifo *fifo, int fflags)
 {
-	//todo
+	struct snes_usb_softc *sc = usb_fifo_softc(fifo);
+	
+	sc->sc_fflags &= ~(fflags & FREAD);
+	if(fflags & FREAD)
+		usb_fifo_free_buffer(fifo);
 }
 
 static int
 snes_usb_ioctl(struct usb_fifo *fifo, u_long cmd, void *data, int fflags)
 {
-	//todo
-	return 0;
+	return (ENODEV);
 }
 
 static void
 snes_usb_watchdog(void *arg)
 {
-	//todo
+	struct snes_usb_softc *sc = arg;
+	mtx_assert(&sc->sc_mutex, MA_OWNED);
+
+	if(sc->sc_fflags == 0)
+		usbd_transfer_start(sc->sc_transfer[SNES_USB_STATUS_DT_RD]);
+
+	usb_callout_reset(&sc->sc_watchdog, hz, &snes_usb_watchdog, sc);
 }
 
 static void 
 snes_usb_start_read(struct usb_fifo *fifo)
 {
-	//todo
+	struct snes_usb_softc *sc = usb_fifo_softc(fifo);
+	usbd_transfer_start(sc->sc_transfer[SNES_USB_INTR_DT_RD]);
 }
 
 static void
 snes_usb_stop_read(struct usb_fifo *fifo)
 {
-	//todo
+	struct snes_usb_softc *sc = usb_fifo_softc(fifo);
+	
+	usbd_transfer_stop(sc->sc_transfer[SNES_USB_INTR_DT_RD]);
 }
 
 static void
 snes_usb_read_callback(struct usb_xfer *transfer, usb_error_t error)
 {
-	//todo
+	struct snes_usb_softc *sc = usbd_xfer_softc(transfer);
+	struct usb_fifo *fifo = sc->sc_fifo_open[USB_FIFO_RX];
+	struct usb_page_cache *pc;
+	int actual, max;
+
+	usbd_xfer_status(transfer, &actual, NULL, NULL, NULL);
+	
+	if(fifo == NULL)
+		return;
+
+	switch(USB_GET_STATE(transfer)){
+	
+		case USB_ST_TRANSFERRED:
+			if(actual == 0){
+				if(sc->sc_zero_length_packets == 4)
+					/*Throttle transfers. */
+					usbd_xfer_set_interval(transfer, 500);
+				else
+					sc->sc_zero_length_packets++;
+
+			}else{
+				/*disable throttling. */
+				usbd_xfer_set_interval(transfer, 0);
+				sc->sc_zero_length_packets = 0;
+			}
+
+			pc = usbd_xfer_get_frame(transfer, 0);
+			usb_fifo_put_data(fifo, pc, 0, actual, 1);
+			/*FALLTHROUGH*/
+
+		case USB_ST_SETUP:
+setup:
+			if(usb_fifo_put_bytes_max(fifo) != 0){
+				max = usbd_xfer_max_len(transfer);
+				usbd_xfer_set_frame_len(transfer, 0, max);
+				usbd_transfer_submit(transfer);
+			}
+			break;
+
+		default:
+			/*disable throttling. */
+			usbd_xfer_set_interval(transfer, 0);
+			sc->sc_zero_length_packets = 0;
+
+			if(error != USB_ERR_CANCELLED){
+				/* Issue a clear-stall request. */
+				usbd_xfer_set_stall(transfer);
+				goto setup;
+			}
+
+			break;
+	}
 }
 
 static void
 snes_usb_status_callback(struct usb_xfer *transfer, usb_error_t error)
 {
-	//todo
+	struct snes_usb_softc *sc = usbd_xfer_softc(transfer);
+	struct usb_device_request req;
+	struct usb_page_cache *pc;
+	uint8_t current_status, new_status;
+
+	switch(USB_GET_STATE(transfer)){
+		case USB_ST_SETUP:
+			req.bmRequestType = UT_READ_CLASS_INTERFACE;
+			req.bRequest = UREQ_GET_PORT_STATUS;
+			USETW(req.wValue, 0);
+			req.wIndex[0] = sc->sc_iface_num;
+			req.wIndex[1] = 0;
+			USETW(req.wLength, 1);
+
+			pc = usbd_xfer_get_frame(transfer, 0);
+			usbd_copy_in(pc, 0, &req, sizeof(req));
+			usbd_xfer_set_frame_len(transfer, 0, sizeof(req));
+			usbd_xfer_set_frame_len(transfer, 1, 1);
+			usbd_xfer_set_frames(transfer, 2);
+			usbd_transfer_submit(transfer);
+
+			break;
+		case USB_ST_TRANSFERRED:
+			pc = usbd_xfer_get_frame(transfer, 1);
+			usbd_copy_out(pc, 0, &current_status, 1);
+
+			current_status = (current_status ^ 48);
+			new_status = current_status & ~sc->sc_previous_status;
+			sc->sc_previous_status = current_status;
+
+			break;
+		default:
+			break;
+		}
+			
 }
 
 static int
 snes_usb_probe(device_t dev)
 {
-	//todo
-	return 0;
+	struct usb_attach_arg *uaa = device_get_ivars(dev);
+	
+	if(uaa->usb_mode != USB_MODE_HOST)
+		return (ENXIO);
+
+	if ((uaa->info.bInterfaceClass == UICLASS_HID))
+		return (BUS_PROBE_SPECIFIC); 
+
+	return (ENXIO);
 }
 
 static int
 snes_usb_attach(device_t dev)
 {
-	//todo
-	return 0;
+	struct usb_attach_arg *uaa = device_get_ivars(dev);
+	struct snes_usb_softc *sc = device_get_softc(dev);
+	struct usb_interface_descriptor *idesc;
+	struct usb_config_descriptor *cdesc;
+	uint8_t alt_index, iface_index = uaa->info.bIfaceIndex;
+	int error, unit = device_get_unit(dev);
+	
+	sc->sc_dev = dev;
+	sc->sc_usb_device = uaa->device;
+	device_set_usb_desc(dev);
+	mtx_init(&sc->sc_mutex, "snes_usb", NULL, MTX_DEF | MTX_RECURSE);
+	usb_callout_init_mtx(&sc->sc_watchdog, &sc->sc_mutex, 0);
+	
+	idesc = usbd_get_interface_descriptor(uaa->iface);
+	alt_index = -1;
+	for(;;){
+		if(idesc == NULL)
+			break;
+	
+		if((idesc->bDescriptorType == UDESC_INTERFACE) &&
+		   (idesc->bLength >= sizeof(*idesc))){
+				if(idesc->bInterfaceNumber != uaa->info.bIfaceNum)
+					break;
+				else{
+					alt_index++;
+					if(idesc->bInterfaceClass == UICLASS_HID) 
+						goto found;
+				}
+		}
+		
+		cdesc = usbd_get_config_descriptor(uaa->device);
+		idesc = (void *)usb_desc_foreach(cdesc, (void *)idesc);
+	}
+	goto detach;
+
+found:
+		if(alt_index){
+			error = usbd_set_alt_interface_index(uaa->device, iface_index, alt_index);
+			if(error)
+				goto detach;
+		}
+
+		sc->sc_iface_num = idesc->bInterfaceNumber;
+		
+		error = usbd_transfer_setup(uaa->device, &iface_index,
+			sc->sc_transfer, snes_usb_config, SNES_USB_N_TRANSFER, sc,
+			&sc->sc_mutex);
+
+		if(error)
+			goto detach;
+
+		device_printf(dev, "using unidirectonal mode\n");
+		
+		error = usb_fifo_attach(uaa->device, sc, &sc->sc_mutex,
+			&snes_usb_fifo_methods, &sc->sc_fifo, unit, -1,
+			iface_index, UID_ROOT, GID_OPERATOR, 0644);
+		if(error)
+			goto detach;
+
+		error = usb_fifo_attach(uaa->device, sc, &sc->sc_mutex,
+				&snes_usb_fifo_methods, &sc->sc_fifo_no_reset, unit, -1,
+				iface_index, UID_ROOT, GID_OPERATOR, 0644);
+		if(error)
+			goto detach;
+
+		mtx_lock(&sc->sc_mutex);
+		snes_usb_watchdog(sc);
+		mtx_unlock(&sc->sc_mutex);
+		return (0);
+
+detach:
+		snes_usb_detach(dev);
+		return (ENOMEM);
 }
 
 static int
 snes_usb_detach(device_t dev)
 {
-	//todo
-	return 0;
+	struct snes_usb_softc *sc = device_get_softc(dev);
+
+	usb_fifo_detach(&sc->sc_fifo);
+	usb_fifo_detach(&sc->sc_fifo_no_reset);
+	
+	mtx_lock(&sc->sc_mutex);
+	usb_callout_stop(&sc->sc_watchdog);
+	mtx_unlock(&sc->sc_mutex);
+
+	usbd_transfer_unsetup(sc->sc_transfer, SNES_USB_N_TRANSFER);
+	usb_callout_drain(&sc->sc_watchdog);
+	mtx_destroy(&sc->sc_mutex);
+
+	return (0);
 }
 
 static device_method_t snes_usb_methods[] = {
-		/*Device interface. */
+		//Device interface. 
 		DEVMETHOD(device_probe, snes_usb_probe),
 		DEVMETHOD(device_attach, snes_usb_attach),
 		DEVMETHOD(device_detach, snes_usb_detach),
